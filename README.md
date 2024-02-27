8aacec86583f375d023897f28f13474d0dab0cd7
Victor Torres <vtorremo@emeal.nttdata.com>
2024-02-27 17:55:55 -0500

    Servicio Windows para Operaciones

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Servicio para Operaciones
+
+Servicio para el monitoreo de Operaciones
+
+
+## Installation
+
+Install Servicio con NodeJS
+
+```bash
+  node app.js install
+```
+
+## Uninstallation
+
+Uninstall Servicio con NodeJS
+
+```bash
+  node app.js uninstall
+```
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`DOMAIN=xxxxx`
+
+`ACCOUNT=xxxxx`
+
+`PASSWORD=xxxxxxxxx`
+
+
+## Authors
+
+- [@vtorresm](https://github.com/vtorresm)
---
+++ src/index.js
@@ -0,0 +1,122 @@
+// service.js
+import { Service } from 'node-windows';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import os from 'os';
+import fs from 'fs';
+import { spawn } from 'child_process';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Ruta al archivo .bat que se ejecutará
+const batFilePath = 'C:\\Bachero\\Operations.bat';
+
+// Obtén __dirname en módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Configuración del servicio
+const svc = new Service({
+  name: 'ResetOperations',
+  description:
+    'Servicio ResetOperations creado con node-windows para monitorear el bachero Operations.bat',
+  script: join(__dirname, 'app.js'), // Ruta al archivo principal de tu aplicación
+});
+
+// Función para obtener la fecha y hora actual como una cadena en el formato YYYY-MM-DD HH:MM:SS
+function getDateTimeString() {
+  const date = new Date();
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+    2,
+    '0'
+  )}-${String(date.getDate()).padStart(2, '0')} ${String(
+    date.getHours()
+  ).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(
+    date.getSeconds()
+  ).padStart(2, '0')}`;
+}
+
+// Función para registrar un mensaje en un archivo .log
+function logToFile(message) {
+  const dateTimeString = getDateTimeString();
+  const logFileName = `C:\\logs\\error-${dateTimeString.slice(0, 10)}.log`;
+  fs.appendFileSync(logFileName, `${dateTimeString} - ${message}\n`);
+}
+
+// Método para ejecutar el archivo .bat
+function executeBatFile() {
+  // Verificar si el archivo .bat existe
+  if (!fs.existsSync(batFilePath)) {
+    logToFile(`El archivo ${batFilePath} no existe.`);
+    return;
+  }
+
+  // Aquí puedes agregar cualquier lógica adicional que necesites, como la duración de la ejecución, etc.
+  const bat = spawn(batFilePath);
+
+  // Manejar eventos de salida y error
+  bat.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  bat.stderr.on('data', (data) => {
+    logToFile(data.toString());
+  });
+
+  bat.on('exit', (code) => {
+    const errorMessage = `Proceso de archivo Operations.bat finalizado con código ${code}`;
+    logToFile(errorMessage);
+    console.log(
+      `Proceso de archivo Operations.bat finalizado con código ${code}`
+      );
+  });
+}
+
+// Configurar el servicio
+svc.on('install', () => {
+  console.log('Servicio instalado correctamente...');
+
+  svc.logOnAs.domain = process.env.DOMAIN;
+  svc.logOnAs.account = process.env.ACCOUNT;
+  svc.logOnAs.password = process.env.PASSWORD;
+
+  // Obtén el nombre del usuario local actual
+  const localUser = os.userInfo().username;
+
+  // Verificar si el usuario local y el dominio coinciden con las variables de entorno
+  if (
+    localUser === process.env.ACCOUNT &&
+    os.hostname() === process.env.DOMAIN
+    ) {
+    // Ejecutar el archivo .bat después de la instalación
+    executeBatFile();
+  } else {
+    // Si no, registra un mensaje de error
+    const errorMessage = `El usuario local (${localUser}) o el dominio (${os.hostname()}) no coinciden con las variables de entorno (${
+      process.env.ACCOUNT
+    }, ${process.env.DOMAIN}).`;
+    logToFile(errorMessage);
+    console.log(
+      `El usuario local (${localUser}) o el dominio (${os.hostname()}) no coinciden con las variables de entorno (${
+        process.env.ACCOUNT
+      }, ${process.env.DOMAIN}).`
+      );
+  }
+});
+
+svc.on('uninstall', () => {
+  console.log('Servicio desinstalado.');
+});
+
+// Instalar o desinstalar el servicio según el argumento de línea de comandos
+if (process.argv[2] === 'install') {
+  svc.install();
+  logToFile('Service installed.');
+} else if (process.argv[2] === 'uninstall') {
+  svc.uninstall();
+  logToFile('Service uninstalled.');
+} else {
+  const errorMessage = `Error: Argumento no válido. Uso correcto: - Para instalar: node app.js install - Para desinstalar: node app.js uninstall`;
+  logToFile(errorMessage);
+}
