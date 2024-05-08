83c3790ef5db636fa82a3f0087a2a4bc91ca4dee
Victor Torres <vatorresm88@gmail.com>
2024-05-08 16:03:05 -0500

    Modificando el logToFile

--- src/common/logToFile.js
+++ src/common/logToFile.js
@@ -1,10 +1,14 @@
 import fs from 'fs';
-
 import { getDateTimeString } from './getDateTimeString.js';
 
 // Función para registrar un mensaje en un archivo .log
 export function logToFile(message) {
   const dateTimeString = getDateTimeString();
   const logFileName = `C:\\logs\\error-${dateTimeString.slice(0, 10)}.log`;
-  fs.appendFileSync(logFileName, `${dateTimeString} - ${message}\n`);
+
+  // Utilizar el método fs.promises.appendFile en lugar de fs.appendFileSync para evitar bloquear el hilo principal
+  fs.promises.appendFile(logFileName, `${dateTimeString} - ${message}\n`)
+    .catch((error) => {
+      console.error(`Error al escribir en el archivo de registro: ${error}`);
+    });
 }
