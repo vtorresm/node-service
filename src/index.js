c1ddaea86bf7cb4b0108ff8973508e8a01628d71
Victor Torres <vatorresm88@gmail.com>
2024-05-09 16:03:45 -0500

    Modificando el index

--- src/index.js
+++ src/index.js
@@ -14,11 +14,14 @@ dotenv.config();
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+// Obtener la ruta del archivo actual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Obtener la ruta del archivo BAT desde las variables de entorno
 const batFilePath = process.env.BATFILEPATH;
 
+// Crear una instancia del servicio de Windows
 const svc = new Service({
   name: 'ServiceMessage',
   description:
@@ -33,82 +36,91 @@ const svc = new Service({
   stopparentfirst: false,
 });
 
+// Función para ejecutar el archivo BAT
 async function executeBatFile() {
+  // Verificar si el archivo BAT existe
   if (!fs.existsSync(batFilePath)) {
-    logToFile(chalk.red(`El archivo ${batFilePath} no existe.`)); // Colorear mensaje de error en rojo
+    logToFile(chalk.red(`El archivo ${batFilePath} no existe.`));
     return;
   }
 
+  // Verificar el estado del servicio
   const status = await checkStatus();
   if (status !== 200) {
     setInterval(() => {
       const bat = spawn(batFilePath);
 
+      // Manejar la salida estándar del proceso BAT
       bat.stdout.on('data', (data) => {
-        console.log(chalk.green(data.toString())); // Colorear mensaje de salida en verde
+        console.log(chalk.green(data.toString()));
       });
 
+      // Manejar la salida de error del proceso BAT
       bat.stderr.on('data', (data) => {
-        logToFile(data.toString()); // Colorear mensaje de error en rojo
+        logToFile(data.toString());
       });
 
+      // Manejar el evento de salida del proceso BAT
       bat.on('exit', (code) => {
         const errorMessage = `Proceso de archivo service.bat finalizado con código ${code}`;
-        logToFile(errorMessage); // Colorear mensaje de advertencia en amarillo
-        console.log(chalk.yellow(errorMessage)); // Colorear mensaje de advertencia en amarillo
+        logToFile(errorMessage);
+        console.log(chalk.yellow(errorMessage));
       });
     }, 0.5 * 60 * 1000 * 1000);
   }
 }
 
+// Manejar el evento de instalación del servicio
 svc.on('install', function (error) {
   if (error) {
     console.log(chalk.red('Error al instalar el servicio:', error));
-    logToFile('Error al instalar el servicio:', error); // Colorear mensaje de error en rojo
+    logToFile('Error al instalar el servicio:', error);
     return;
   }
 
-  console.log(chalk.blue('Servicio se ha instalado correctamente...')); // Colorear mensaje de información en azul
+  console.log(chalk.blue('Servicio se ha instalado correctamente...'));
 
+  // Configurar las credenciales de inicio de sesión del servicio
   svc.logOnAsDomain = process.env.DOMAIN;
   svc.logOnAsAccount = process.env.ACCOUNT;
   svc.logOnAsPassword = process.env.PASSWORD;
 
+  // Obtener el usuario local y el nombre de host
   const localUser = os.userInfo().username;
+  const hostname = os.hostname();
 
-  if (
-    localUser === process.env.ACCOUNT &&
-    os.hostname() === process.env.DOMAIN
-  ) {
+  // Verificar si el usuario local y el nombre de host coinciden con las variables de entorno
+  if (localUser === process.env.ACCOUNT && hostname === process.env.DOMAIN) {
     executeBatFile();
   } else {
-    const errorMessage = `El usuario local (${localUser}) o el dominio (${os.hostname()}) no coinciden con las variables de entorno (${
-      process.env.ACCOUNT
-    }, ${process.env.DOMAIN}).`;
-    logToFile(errorMessage); // Colorear mensaje de error en rojo
-    console.log(chalk.red(errorMessage)); // Colorear mensaje de error en rojo
+    const errorMessage = `El usuario local (${localUser}) o el dominio (${hostname}) no coinciden con las variables de entorno (${process.env.ACCOUNT}, ${process.env.DOMAIN}).`;
+    logToFile(errorMessage);
+    console.log(chalk.red(errorMessage));
   }
 });
 
+// Manejar el evento de desinstalación del servicio
 svc.on('uninstall', function (error) {
   if (error) {
     console.log(chalk.red('Error al desinstalar el servicio:', error));
     return;
   }
 
-  console.log(chalk.blue('Servicio desinstalado.')); // Colorear mensaje de información en azul
+  console.log(chalk.blue('Servicio desinstalado.'));
 });
 
+// Verificar los argumentos de línea de comandos para instalar o desinstalar el servicio
 if (process.argv[2] === 'install') {
   svc.install();
-  console.log(chalk.green('Service installed.')); // Colorear mensaje de éxito en verde
-  logToFile('Service installed.'); // Colorear mensaje de éxito en verde
+  console.log(chalk.green('Service installed.'));
+  logToFile('Service installed.');
 } else if (process.argv[2] === 'uninstall') {
   svc.uninstall();
-  console.log(chalk.green('Service uninstalled.')); // Colorear mensaje de éxito en verde
-  logToFile('Service uninstalled.'); // Colorear mensaje de éxito en verde
+  console.log(chalk.green('Service uninstalled.'));
+  logToFile('Service uninstalled.');
 } else {
-  const errorMessage = `Error: Argumento no válido. Uso correcto: - Para instalar: node app.js install - Para desinstalar: node app.js uninstall`;
-  console.log(chalk.red(errorMessage)); // Colorear mensaje de error en rojo
-  logToFile(errorMessage); // Colorear mensaje de error en rojo
+  const errorMessage =
+    'Error: Argumento no válido. Uso correcto: - Para instalar: node app.js install - Para desinstalar: node app.js uninstall';
+  console.log(chalk.red(errorMessage));
+  logToFile(errorMessage);
 }
